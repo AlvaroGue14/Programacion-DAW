63cb91f2005fb943f0bb08c7ac40dd6ea482576d
AlvaroGue14 <al.guevara1408@gmail.com>
2025-09-24 14:20:28 +0200

    Primera clase de fundamentos

--- Fundamentos JAVA/src/App.java
+++ Fundamentos JAVA/src/App.java
@@ -1,5 +1,44 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String cadena="Me llamo Alvaro";
+        int edad=18;
+        double salario=20.50; 
+
+            //int numero entero
+            //double numero decimal/real
+            //char caracter
+            //boolean true or false
+
+        int a=(int)2.6;         //pasa el numero decimal a numero entero
+        System.out.println(a);
+        
+        double b=(int)2.6;      
+        System.out.println(b);
+        
+        int uno=1/3;
+        System.out.println(uno);
+
+        double dos=1/3.0;
+        System.out.println(dos);
+
+        final double PI=3.14;
+        System.out.println(PI);
+            
+        int aa='a'; //un caracter se guarda en un entero segun la tabla ASCII
+        char c=98;
+        System.out.print(aa+" "+c);
+        
     }
+        
+        /*
+         * Programa para imprimir texto
+         * @author Alvaro Guevara
+         * 
+         */
+
+        public static void imprimirTexto(String texto){
+            boolean logico=false;
+
+        }
+
 }
