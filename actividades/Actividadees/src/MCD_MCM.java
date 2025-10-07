18607d9348d66027aa3b8c54626a1bc6c081c8a4
AlvaroGue14 <al.guevara1408@gmail.com>
2025-10-07 13:07:35 +0200

    Deberes Java

--- actividades/Actividadees/src/MCD_MCM.java
+++ actividades/Actividadees/src/MCD_MCM.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class MCD_MCM {
     
     public static void main(String[] args) {
         
-        
+        int a;
+        int b;
+        int resto;
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Introduce a");
+        a=teclado.nextInt();
+        System.out.println("Introduce b");
+        b=teclado.nextInt();
+        int originalA= a;
+        int originalB= b;
+        while (b!=0) {
+            resto=a%b;
+            a=b;
+            b=resto;
+        }
+        System.out.println("El MCD es "+a);
+
+
+        //MCM
+        int mcm=Math.max(originalA,originalB);
+        while (mcm % originalA!=0 || mcm % originalB!=0) {
+            mcm+=Math.max(originalA, originalB);
+        }
+            System.out.println("EL MCM es "+mcm);
     }
 }
